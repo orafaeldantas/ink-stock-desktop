ea1eca0432eb32d44bcfe1a8e7a0cb750fda677b
Rafael Dantas <rafael.hfdantas@outlook.com>
2025-05-10 13:07:53 -0300

    up

--- services/stock_service.py
+++ services/stock_service.py
@@ -12,13 +12,20 @@ def list_colors_by_model(id_models):
     con = connect()
     cur = con.cursor()
     cur.execute("""
-        SELECT c.id, c.name. e.amount
+        SELECT c.id, c.name. s.amount
         FROM colors c
-        LEFT JOIN stock e ON e.id_colors = c.id
+        LEFT JOIN stock s ON s.id_colors = c.id
         WHERE c.id_models = ? 
-    """, (id_models,))
+    """, (id_models))
     data = cur.fetchall()
     con.close()
     return data
+
+def update_quantity(new_amount, id_colors):
+    con = connect()
+    cur = con.cursor()
+    cur.execute("UPDATE stock SET amount = ? WHERE id_colors = ?", (new_amount, id_colors))
+    con.commit()
+    con.close()
     
     
