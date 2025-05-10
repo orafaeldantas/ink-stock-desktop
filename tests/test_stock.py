aa331e4ecb52c295ec2b828d666675bcbd7dcffd
Rafael Dantas <rafael.hfdantas@outlook.com>
2025-05-10 15:24:02 -0300

    feat(db): add database code

--- tests/test_stock.py
+++ tests/test_stock.py
@@ -1,3 +1,49 @@
 from services.stock_service import list_colors_by_model, update_quantity
 
+def display_colors(id_models):
+    colors = list_colors_by_model(id_models)
+    print(f"\nCores do modelo {id_models}: ")
+    for color in colors:
+        id_colors, name, amount = color
+        amount = amount if amount is not None else 0
+        print(f"ID: {id_colors} | Nome: {name} | Quantidade: {amount}")
+    
+    return colors
+
+def get_current_quantity(id_colors, colors):
+    for color in colors:
+        if color[0] == id_colors:
+            return color[0] if color[2] is not None else 0
+    
+    return 0
+
+def main():
+    id_models = int(input("Digite o ID do modelo (1 a 4): "))
+
+    color = display_colors(id_models)
+
+    id_colors = int(input("\nDigite o ID da cor que deseja movimentar: "))
+    operation = input("Digite 'e' para entrada ou 's' para saída: ")
+
+    value = int(input("Quantidade a movimentar: "))
+    actualy = get_current_quantity(id_colors, color)
+
+    if operation == 'e':
+        new = actualy + value
+    elif operation == 's':
+        if value > actualy:
+            print("Erro: Não há estoque suficiente para essa saída.")
+            return
+        new = actualy - value
+    else:
+        print("Operação inválida.")
+        return
+    
+    update_quantity(id_colors, new)
+    print("\nEstoque atualizado com sucesso!")
+
+    display_colors(id_models)
+
+if __name__ == "__main__":
+    main()
 
