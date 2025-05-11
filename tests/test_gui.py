352d7ea524b5791df16e68ebd9f16450aea82888
Rafael Dantas <rafael.hfdantas@outlook.com>
2025-05-11 11:32:23 -0300

    up

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -5,29 +5,56 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import customtkinter as ctk
 
+
 def create_main_window():
-    # Criar a janela principal
+    ctk.set_appearance_mode("System")  # Opcional: "Light", "Dark", "System"
+    ctk.set_default_color_theme("blue")  # Pode ser "green", "dark-blue", etc.
+
     window = ctk.CTk()
-    window.title("Minha Janela CustomTkinter")
+    window.title("Abas com CustomTkinter")
     window.geometry("1200x800")
 
-    # Adicionar um botão simples
-    def on_button_click():
-        print("Botão clicado!")
+    # Criar o Tabview (as abas)
+    tabview = ctk.CTkTabview(master=window)   
+    tabview.pack(padx=20, pady=20, fill="both", expand=True)
 
-    button = ctk.CTkButton(window, text="Clique aqui", command=on_button_click)
-    button.pack(pady=20)
+    # Adicionar abas
+    tabview.add("Konica")
+    tabview.add("DX")    
+    tabview.add("UV Rolo")
+    tabview.add("UV Mesa")
+    tabview.add("Configurações")
 
-    # Adicionar um campo de entrada de texto
-    entry = ctk.CTkEntry(window)
-    entry.pack(pady=20)
+    for tab_button in tabview._segmented_button._buttons_dict.values():
+        tab_button.configure(font=("Arial", 30))
 
-    # Adicionar um label
-    label = ctk.CTkLabel(window, text="Texto simples no Label")
-    label.pack(pady=20)
+    # Pegar referências para cada aba
+    tabKonica = tabview.tab("Konica")
+    tabDX = tabview.tab("DX")
+    tabUVRolo = tabview.tab("UV Rolo")
+    tabUVMesa = tabview.tab("UV Mesa")
+    config = tabview.tab("Configurações")
 
-    # Exibir a janela
+    # Adicionar conteúdo em cada aba
+    labelKonica = ctk.CTkLabel(tabKonica, text="Konica")
+    labelKonica.pack(pady=10)
+
+    labelDX = ctk.CTkLabel(tabDX, text="DX")
+    labelDX.pack(pady=10)
+
+    labelUVRolo = ctk.CTkLabel(tabUVRolo, text="UV Rolo")
+    labelUVRolo.pack(pady=10)
+
+    labelUVMesa = ctk.CTkLabel(tabUVMesa, text="UV Mesa")
+    labelUVMesa.pack(pady=10)
+
+
+
+    checkbox = ctk.CTkCheckBox(config, text="Ativar notificações")
+    checkbox.pack(pady=10)
+
     window.mainloop()
+
 
 if __name__ == "__main__":
     create_main_window()
